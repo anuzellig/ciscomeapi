2b147b573c31584131163ca9a5df523e42c1f6cb
Greg Anuzelli <{ID}+{username}@users.noreply.github.com>
2019-11-25 16:14:51 -0500

    Bump version in __init__

--- ciscomeapi/__init__.py
+++ ciscomeapi/__init__.py
@@ -2,5 +2,5 @@ from .ciscomeapi import CiscoME, print_table
 
 __author__     = "Greg Anuzelli"
 __copyright__  = "Copyright (c) 2019 Greg Anuzelli"
-__version__    = "0.1"
+__version__    = "0.2"
 __status__     = "Development"
