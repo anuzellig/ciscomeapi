47a3d3a08f7aafe65f0e2b68a0b1699f356b264d
Greg Anuzelli <{ID}+{username}@users.noreply.github.com>
2019-11-27 10:50:00 -0500

    Changed documentation theme

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,8 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 sys.setrecursionlimit(1500)
 
+import sphinx_rtd_theme
+
 import ciscomeapi
 
 # -- Project information -----------------------------------------------------
@@ -32,7 +34,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,7 +50,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
